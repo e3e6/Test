9cc59863907364af7139ff549b3401801fc7c4e1
alex.z <alexander.zrelykh@creamtec.com>
2015-06-06 12:34:10 +0300

    Working Hard

---
+++ src/com/sourceit/java/basic/gitTest/WorkingHard.java
@@ -0,0 +1,12 @@
+package com.sourceit.java.basic.gitTest;
+
+public class WorkingHard {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Working Hard");
+	}
+
+}
